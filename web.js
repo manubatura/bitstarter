806ce1a5e4eaac2cc2cf2f4e1ac7666e279a8590
Ubuntu <ubuntu@ip-172-31-1-149.us-west-2.compute.internal>
2013-09-15 20:25:10 +0000

    Added file read function

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buff = fs.readFileSync('index.html');
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
